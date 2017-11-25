7ab7541a25e1747a68059d1a4e71111bd37b35a6
Simen <slunke@gmail.com>
2017-11-25 20:22:54 +0100

    Add files via upload

---
+++ Attiny_flash.ino
@@ -0,0 +1,26 @@
+int ledpin = 4;
+int buttonpin = 3;
+
+void setup() {
+  pinMode(ledpin, OUTPUT);
+  pinMode(buttonpin, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(buttonpin) == LOW){
+    flash(3);
+    delay(500);
+    flash(6);
+  }
+}
+
+
+void flash(int count){
+  for (int i=0;i< count;i++){
+    digitalWrite(ledpin, HIGH);
+    delay(10);
+    digitalWrite(ledpin, LOW);
+    delay(100);
+  }
+}
+
